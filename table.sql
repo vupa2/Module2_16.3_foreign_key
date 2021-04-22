660fc7a01c92abf257eac54dcd4a8fcdf663f9fb
vupa2 <anhvu.pa96@gmail.com>
2021-04-22 16:54:53 +0700

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS orders, customers;
+
+CREATE TABLE customers
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40),
+    address VARCHAR(255),
+    email VARCHAR(255)
+);
+
+CREATE TABLE orders(
+   id INT AUTO_INCREMENT,
+   staff VARCHAR(50),
+   PRIMARY KEY(id),
+   customer_id INT,
+   FOREIGN KEY (customer_id) REFERENC
